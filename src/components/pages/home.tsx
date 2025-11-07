bb3ad1a282c649ac79169e628e288165aacda46b
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2025-11-07 10:48:58 +0100

    add Home component with Hero and Chat subcomponents

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Chat from '../shared/chat/chat'
+import Hero from '../shared/hero/hero'
 
 export default function Home() {
   return (
     <div>
-      
+      <Hero />
+      <Chat />
     </div>
   )
 }
