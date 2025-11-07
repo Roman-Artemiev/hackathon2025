893da006421d9d71822d51155fd09ef1a8085d52
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2025-11-07 14:50:01 +0100

    add simple styles

--- src/components/shared/chat/ChatGrid.tsx
+++ src/components/shared/chat/ChatGrid.tsx
@@ -1,36 +1,25 @@
 ﻿import type GridItem from "@/structures/GridItem";
 
-interface IChatGrid{
+interface IChatGrid {
     gridItems: GridItem[];
 }
 
 
 
-export default function ChatGrid({gridItems} : IChatGrid){
-    return(
-        <>
-        <div className="grid-cols-2 grid">
-
-            {
-                gridItems?.map((item)=>(
-                <>
-                {item.author == 0 && <>
-                <div className="col-span-2 bg-amber-700">
+export default function ChatGrid({ gridItems }: IChatGrid) {
+    return (
+        <div className="grid-cols-2 grid gap-1">
+            {gridItems?.map((item, i) => (
+                <div
+                    key={i}
+                    className={`max-w-[75%] py-2 px-3 rounded-xl text-sm leading-relaxed ${item.author === 0
+                        ? "col-span-2 justify-self-start bg-gray-100 text-gray-800 rounded-bl-none"
+                        : "col-span-2 justify-self-end bg-orange-100 text-gray-900 rounded-br-none text-right"
+                        }`}
+                >
                     {item.text}
-                </div>    
-                </>}
-                {item.author == 1 && <>
-                <div className="col-span-2 col-start-2 col-end-3 self-end content-end">
-                    {item.text}
-                </div>    
-                </>}
-                </>  
-                ))
-            }
-
-            
-            
+                </div>
+            ))}
         </div>
-        </>
     );
 }
--- src/components/shared/chat/ChatSubHeading.tsx
+++ src/components/shared/chat/ChatSubHeading.tsx
@@ -1,11 +1,11 @@
 ﻿import React from 'react'
 
-interface ChatSubHeadingProp{
-    text : string;
+interface ChatSubHeadingProp {
+  text: string;
 }
 
-export default function ChatSubHeading({text} : ChatSubHeadingProp) {
+export default function ChatSubHeading({ text }: ChatSubHeadingProp) {
   return (
-    <span>{text}</span>
+    <div className='max-w-[75%] py-2 px-3 rounded-xl text-sm leading-relaxed col-span-2 justify-self-start bg-gray-100 text-gray-800 rounded-bl-none'>{text}</div>
   )
 }
