029d6a6b926ae52507412a08786de16f0fcabc52
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2025-11-07 13:20:09 +0100

    move prompt bar to chat

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,13 +1,11 @@
 import Chat from '../shared/chat/chat'
 import Hero from '../shared/hero/hero'
-import Prompt from '../shared/prompt/prompt'
 
 export default function Home() {
   return (
     <div className='px-10 w-full'>
       <Hero />
       <Chat />
-      <Prompt />
     </div>
   )
 }
--- src/components/shared/chat/chat.tsx
+++ src/components/shared/chat/chat.tsx
@@ -4,6 +4,7 @@ import ChatGrid from "./ChatGrid";
 import ChatHeading from "./ChatHeading";
 import ChatSubHeading from "./ChatSubHeading";
 import AuthorType from "@/structures/AuthorType";
+import PromptBar from "../prompt/prompt-bar";
 
 
 
@@ -12,41 +13,43 @@ import AuthorType from "@/structures/AuthorType";
 export default function Chat() {
 
   const gridItems = [
-        {
-          text : "Bot",
-          author : 0,
-        },
-        {
-          text : "Author",
-          author : 1,
-        },
-        {
-          text : "Bot",
-          author : 0,
-        }
-      ];
+    {
+      text: "Bot",
+      author: 0,
+    },
+    {
+      text: "Author",
+      author: 1,
+    },
+    {
+      text: "Bot",
+      author: 0,
+    }
+  ];
 
 
   const [userPrompt, setUserPrompt] = useState<string>();
-  
+
   return (
     <>
       <div className="w-full h-fit bg-amber-400">
 
         <ChatHeading headningText="Zdravim" />
 
-        <ChatSubHeading text="Podtext "/>
+        <ChatSubHeading text="Podtext " />
 
-        <ChatGrid gridItems={gridItems??[]} />
+        <ChatGrid gridItems={gridItems ?? []} />
 
         {/* <ChatQuickResponses  /> */}
 
-        <InputChatBlock text={userPrompt} color="" textSetter={setUserPrompt}/>
+        <InputChatBlock text={userPrompt} color="" textSetter={setUserPrompt} />
 
-        
+        <div className='rounded-2xl mx-auto max-w-4xl'>
+          <PromptBar />
+        </div>
       </div>
-      
+
     </>
-    
+
   )
 }
