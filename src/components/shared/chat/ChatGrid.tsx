f8ed94eb5395c4bd8b7dd4591b914e1e5875204c
Michal Špitálský <michal.spitalsky@pm.me>
2025-11-07 14:53:29 +0100

    feat (ui): added a thinking placeholder bubble

--- src/components/shared/chat/ChatGrid.tsx
+++ src/components/shared/chat/ChatGrid.tsx
@@ -1,25 +1,36 @@
 ﻿import type GridItem from "@/structures/GridItem";
 
 interface IChatGrid {
-    gridItems: GridItem[];
+  gridItems: GridItem[];
 }
 
-
-
 export default function ChatGrid({ gridItems }: IChatGrid) {
-    return (
-        <div className="grid-cols-2 grid gap-1">
-            {gridItems?.map((item, i) => (
-                <div
-                    key={i}
-                    className={`max-w-[75%] py-2 px-3 rounded-xl text-sm leading-relaxed ${item.author === 0
-                        ? "col-span-2 justify-self-start bg-gray-100 text-gray-800 rounded-bl-none"
-                        : "col-span-2 justify-self-end bg-orange-100 text-gray-900 rounded-br-none text-right"
-                        }`}
-                >
-                    {item.text}
-                </div>
-            ))}
+  return (
+    <div className="grid-cols-2 grid gap-1">
+      {gridItems?.map((item, i) => (
+        <div
+          key={i}
+          className={`max-w-[75%] py-2 px-3 rounded-xl text-sm leading-relaxed ${
+            item.author === 0
+              ? "col-span-2 justify-self-start bg-gray-100 text-gray-800 rounded-bl-none"
+              : "col-span-2 justify-self-end bg-orange-100 text-gray-900 rounded-br-none text-right"
+          }`}
+        >
+          {item.text}
         </div>
-    );
+      ))}
+    </div>
+  );
+}
+
+export function ThinkingChatGrid() {
+  return (
+    <div className="grid-cols-2 grid gap-1">
+      <div
+        className={`max-w-[75%] py-2 px-3 rounded-xl text-sm leading-relaxed col-span-2 justify-self-start bg-gray-200 text-gray-800 rounded-bl-none font-black`}
+      >
+        <p className="animate-pulse text-3xl m-0">...</p>
+      </div>
+    </div>
+  );
 }
--- src/components/shared/chat/chat.tsx
+++ src/components/shared/chat/chat.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import ChatGrid from "./ChatGrid";
+import ChatGrid, { ThinkingChatGrid } from "./ChatGrid";
 import ChatHeading from "./ChatHeading";
 import ChatSubHeading from "./ChatSubHeading";
 import PromptBar from "../prompt/prompt-bar";
@@ -18,36 +18,42 @@ export default function Chat() {
     {
       text: "Bot",
       author: 0,
-    }
-  ])
+    },
+  ]);
 
   const chatContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Scroll to bottom whenever messages change
     if (chatContainerRef.current) {
-      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
     }
   }, [gridItems]); // run effect when messages array changes
 
   chatContainerRef?.current?.scrollTo({
     top: chatContainerRef.current.scrollHeight,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 
   return (
     <div className="mb-20 w-full h-fit mx-auto max-w-4xl min-h-96">
       <ChatHeading headningText="Zdravim" />
 
-      <div className="space-y-1 p-4 rounded-2xl bg-white mb-6 max-h-96 h-96 overflow-auto scrollbar" ref={chatContainerRef}>
+      <div
+        className="space-y-1 p-4 rounded-2xl bg-white mb-6 max-h-96 h-96 overflow-auto scrollbar"
+        ref={chatContainerRef}
+      >
         <ChatSubHeading text="Podtext " />
 
         <ChatGrid gridItems={gridItems ?? []} />
+
+        <ThinkingChatGrid />
       </div>
 
-      <div className='rounded-2xl '>
+      <div className="rounded-2xl ">
         <PromptBar setGridItems={setGridItems} gridItems={gridItems} />
       </div>
     </div>
-  )
+  );
 }
